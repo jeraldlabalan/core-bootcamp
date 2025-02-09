67c5553696f2e0462cd2c2192a6686991b860590
Jerald Labalan <jeraldbalan26@gmail.com>
2025-02-09 09:27:38 +0800

    asdasdasdasdas

--- 02-hardhat/contracts/Lock.sol
+++ 02-hardhat/contracts/Lock.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.28;
+pragma solidity >=0.8.28 < 0.9.0;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+Uncomment this line to use console.log
+import "hardhat/console.sol";
 
 contract Lock {
     uint public unlockTime;
